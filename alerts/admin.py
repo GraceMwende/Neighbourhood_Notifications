95a0c7aaeb2ff35593545673c51095d684241f93
GraceMwende <grace.micheni@student.moringaschool.com>
2022-03-19 17:17:22 +0300

    register models to admin

--- alerts/admin.py
+++ alerts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile,Users,Neighbourhood,Business
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Users)
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
