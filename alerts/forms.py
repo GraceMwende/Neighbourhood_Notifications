51a98c0a512755d66524c15691011e80dc640cb2
GraceMwende <grace.micheni@student.moringaschool.com>
2022-03-21 10:55:24 +0300

    create forms

--- alerts/forms.py
+++ alerts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+from .models import Profile,Post
 
 
 # class UserRegisterForm(UserCreationForm):
@@ -19,8 +19,13 @@ from .models import Profile
 #         model = User
 #         fields = ['username', 'email']
 
+
 # Create a ProfileUpdateForm to update image
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user','pub_date','comments']
--- alerts/views.py
+++ alerts/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Neighbourhood,Post
+from .forms import NewPostForm
 
 # Create your views here.
 def home(request):
-  return render(request,'home.html')
+  neighbourhoods = Neighbourhood.display_neighbourhhoods()
+  posts = Post.display_posts()
+  return render(request,'home.html',{"neighbourhoods":neighbourhoods,"posts":posts})
+
+def new_post(request):
+  current_user = request.user
+  if request.method == 'POST':
+    form = NewPostForm(request.POST,request.FILES)
+
+    if form.is_valid():
+      post = form.save(commit=False)
+      post.user = current_user
+      # post.profile = Profile.objects.filter(user = current_user).first()
+      post.save()
+    
+    return redirect('home')
+
+  else:
+    form = NewPostForm()
+  return render(request, 'new_post.html',{'form':form})
